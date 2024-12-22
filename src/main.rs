4a87e80b58538ac09ad782fd167d3bd46f6c39f0
webdev219 <aleksaggcweb@gmail.com>
2024-12-22 09:14:15 -0500

    hash map

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ fn add_fancy_hat() {}
 fn remove_fancy_hat() {}
 fn reroll() {}
 
+use std::collections::HashMap;
+
 fn main() {
     let dice_roll = 9;
     match dice_roll {
@@ -26,5 +28,40 @@ fn main() {
         7 => remove_fancy_hat(),
         _ => reroll(),
     }
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 20);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("{score}");
+   
+    let field_name = String::from("Favorite color");
+    let field_value = 32;
+
+    scores.insert(field_name, field_value);
+    scores.insert(String::from("Blue"), 25);
+
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Red")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    let text = "hello world wonderful world";
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    for word in text.split_whitespace() {
+        let count = scores.entry(word.to_string()).or_insert(0);
+
+        println!("{count}");
+        *count += 1;
+    }
+    println!("{scores:?}");
 
 }
