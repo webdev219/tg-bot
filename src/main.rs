765179cb48922056b1d51b15dd6b7a3049863a53
webdev219 <aleksaggcweb@gmail.com>
2025-01-20 07:19:54 +0100

    add aggregator

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,44 @@
-struct Point<T> {
-    x: T,
-    y: T,
-}
+// pub trait Summary {
+//     fn summarize(&self) -> String;
+// }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-}
+// pub struct NewsArticle {
+//     pub headline: String,
+//     pub location: String,
+//     pub author: String,
+//     pub content: String,
+// }
+
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!("{}, by {} ({})", self.headline, self.author, self.location)
+//     }
+// }
+
+// pub struct Tweet {
+//     pub username: String,
+//     pub content: String,
+//     pub reply: bool,
+//     pub retweet: bool,
+// }
+
+// impl Summary for Tweet {
+//     fn summarize(&self) -> String {
+//         format!("{}: {}", self.username, self.content)
+//     }
+// }
+
+use aggregator::{Summary, Tweet};
 
 fn main() {
-    let p = Point { x: 5, y: 10 };
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
 
-    println!("p.x = {}", p.x());
+    println!("1 new tweet: {}", tweet.summarize());
 }
