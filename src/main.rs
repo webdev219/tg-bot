667f41e307aff2d490e0edf401f1bb664ae02c3f
webdev219 <aleksaggcweb@gmail.com>
2025-01-21 07:52:00 +0100

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,18 @@
-// pub trait Summary {
-//     fn summarize(&self) -> String;
-// }
-
-// pub struct NewsArticle {
-//     pub headline: String,
-//     pub location: String,
-//     pub author: String,
-//     pub content: String,
-// }
-
-// impl Summary for NewsArticle {
-//     fn summarize(&self) -> String {
-//         format!("{}, by {} ({})", self.headline, self.author, self.location)
-//     }
-// }
-
-// pub struct Tweet {
-//     pub username: String,
-//     pub content: String,
-//     pub reply: bool,
-//     pub retweet: bool,
-// }
-
-// impl Summary for Tweet {
-//     fn summarize(&self) -> String {
-//         format!("{}: {}", self.username, self.content)
-//     }
-// }
-
-use aggregator::{Summary, Tweet};
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
-        reply: false,
-        retweet: false,
-    };
+    let my_string = String::from("hello world");
 
-    println!("1 new tweet: {}", tweet.summarize());
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
